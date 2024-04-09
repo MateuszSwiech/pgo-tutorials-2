7ae6140c4dfaef412146bd84054c9f6de5861310
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-04-09 11:29:41 +0200

    Exercise 2

--- PGO-2/src/StudentGroup.java
+++ PGO-2/src/StudentGroup.java
@@ -1,4 +1,18 @@
 public class StudentGroup {
     public String nazwa;
 
+    public Student[] students;
+    public int studentCount = 0;
+    public void dodajStudenta(Student student) {
+        if (studentCount >= 15) {
+            throw new IllegalStateException("Grupa jest pełna, max 15 os");
+        }
+        for (int i = 0; i < studentCount; i++) {
+            if (students[i].equals(student)) {
+                throw new IllegalStateException("Ten student został już dodany do grupy.");
+            }
+        }
+        students[studentCount++] = student;
+    }
+
 }
