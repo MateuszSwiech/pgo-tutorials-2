5f7cf6c03f094602446da36a6b6c58f131ae1bd5
MateuszSwiech <163740794+MateuszSwiech@users.noreply.github.com>
2024-04-09 11:27:30 +0200

    Update Main.java

--- PGO-2/src/Main.java
+++ PGO-2/src/Main.java
@@ -1,23 +1,28 @@
-
 public class Main {
     public static void main(String[] args) {
-        Student st = new Student();
-        st.fname = "Adam";
-        st.lname = "Golab";
-        st.grades = new double[]{4,4,4,3};
+        Student st1 = new Student();
+        st1.fname = "Adam";
+        st1.lname = "Golab";
+        st1.grades = new double[]{4,3,2};
+        st1.indexNumber="s30332";
 
-        double srednia = 0;
-        double[] zaokragloneOceny = {2,2.5,3,3.5,4,4.5,5};
-        if (st.grades.length == 0) {
-        throw new IllegalArgumentException("Sudent nie ma żadnej oceny");
+        Student st2 = new Student();
+        st2.fname = "Jan";
+        st2.lname = "Lasek";
+        st2.grades = new double[]{2,5,3};
+        st2.indexNumber="s34234";
 
-        } else {
-            for (int i = 0; i < st.grades.length; i++) {
-                srednia += st.grades[i];
+        StudentGroup stg= new StudentGroup();
+        stg.nazwa = "Grupa 1";
+        stg.students = new Student[15];
 
-            }
-            srednia /= st.grades.length;
-            System.out.println(srednia);
-        }
+        stg.dodajStudenta(st1);
+        stg.dodajStudenta(st2);
+
+        double sr1 =st1.obliczanieSredniej();
+        System.out.println(sr1);
+        double sr2 =st2.obliczanieSredniej();
+        System.out.println(sr2);
+
     }
 }
